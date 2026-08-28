fb38a9b7feec77e9fe9736f6de9e8a7d2e329171
bluehawhy <bluehawhy@gmail.com>
2026-08-29 01:24:23 +0900

    qq

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ function HomePage() {
           <View style={styles.characterContainer}>
             <Image
               source={{
-                uri: 'https://github.com/bluehawhy/card-forge/blob/main/assets/images/index/index.jpg?raw=true',
+                uri: 'https://github.com/bluehawhy/card-forge/blob/main/assets/images/index/characters/rose.jpg?raw=true',
               }}
               style={styles.characterImage}
               resizeMode="cover"
